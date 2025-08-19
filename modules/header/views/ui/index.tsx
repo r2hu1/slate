3c427a3a857c35010fc4c9251149edfce24a0e41
r2hu1 <newasram86@gmail.com>
2025-08-19 20:10:49 +0530

    feat: fixed dynamic nav

--- modules/header/views/ui/index.tsx
+++ modules/header/views/ui/index.tsx
@@ -4,16 +4,24 @@ import Breadcrumbs from "./breadcrumbs";
 import { ModeToggle } from "@/components/theme-switcher";
 import DynamicNav from "./dynamic-nav";
 import Tooltip from "@/components/ui/tooltip-v2";
+import { cn } from "@/lib/utils";
 
 export default function DashboardHeader() {
-  const { open } = useSidebar();
-  return (
-    <header className="px-4 h-14 flex items-center gap-2">
-      {!open && <SidebarTrigger />}
-      <div className="flex md:hidden">
-        <SidebarTrigger />
-      </div>
-      <DynamicNav />
-    </header>
-  );
+	const { open } = useSidebar();
+	return (
+		<div className="mb-14">
+			<header
+				className={cn(
+					"px-4 h-14 flex items-center gap-2 transition-all right-0 fixed top-0 w-full",
+					open && "md:w-[calc(100%-260px)]",
+				)}
+			>
+				{!open && <SidebarTrigger />}
+				<div className="flex md:hidden">
+					<SidebarTrigger />
+				</div>
+				<DynamicNav />
+			</header>
+		</div>
+	);
 }
