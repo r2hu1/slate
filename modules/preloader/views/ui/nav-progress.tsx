7538ed87fcef2166881693b4cddfd968e62f3e1b
r2hu1 <newasram86@gmail.com>
2025-08-23 22:53:53 +0530

    upd

--- modules/preloader/views/ui/nav-progress.tsx
+++ modules/preloader/views/ui/nav-progress.tsx
@@ -9,12 +9,11 @@ export default function NavProgress() {
 	const { status } = useNavigationProgress({
 		debug: false,
 	});
-
 	return (
 		<div
 			className={cn(
 				"py-2 px-3.5 flex cursor-progress items-center transition duration-200 justify-center rounded-t-md fixed bottom-0 left-1/2 -translate-x-1/2 z-[999] bg-primary text-primary-foreground translate-y-0",
-				status === "idle" && "translate-y-full",
+				status !== "loading" && "translate-y-full",
 			)}
 		>
 			<div className="flex items-center gap-2">
